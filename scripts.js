4bc5849c6b83a2b1d4d88c7bf598626282fbfbdd
Togu Naveen <108080993+naveen-togu@users.noreply.github.com>
2023-05-13 10:07:06 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+randomNumber1=Math.floor(Math.random()*6)+1
+randomNumber2=Math.floor(Math.random()*6)+1
+document.querySelector(".img1").setAttribute("src","images/dice"+randomNumber1+".png")
+document.querySelector(".img2").setAttribute("src","images/dice"+randomNumber2+".png")
+if(randomNumber1===randomNumber2){
+    document.querySelector("h1").textContent="Draw!";
+}
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").textContent="🚩Player1 Wins!";
+}
+if(randomNumber1<randomNumber2){
+    document.querySelector("h1").textContent="Player2 Wins!🚩";
+}
